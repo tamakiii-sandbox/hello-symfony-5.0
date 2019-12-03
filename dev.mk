43a559fb12d324ba3593e149ee2800ef7cbce296
tamakiii <tamakiii@users.noreply.github.com>
2019-12-03 21:40:24 +0900

    using nfs

--- dev.mk
+++ dev.mk
@@ -1,7 +1,10 @@
 .PHONY: all clean
 
+DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+
 ENVIRONMENT := development
 PORT_HTTP := 8080
+NFS_DEVICE_DIR ?= $(shell echo $(DIR) | sed 's|/Users|/System/Volumes/Data/Users|')
 
 all:
 	cat $(lastword $(MAKEFILE_LIST))
@@ -10,6 +13,7 @@ all:
 	touch $@
 	echo "ENVIRONMENT=$(ENVIRONMENT)" >> $@
 	echo "PORT_HTTP=$(PORT_HTTP)" >> $@
+	echo "NFS_DEVICE_DIR=$(NFS_DEVICE_DIR)" >> $@
 
 clean:
 	rm -rf app
