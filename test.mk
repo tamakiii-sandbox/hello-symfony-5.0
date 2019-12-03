a79ed0ec83976485cc540e1eaf64de1ca49da25b
tamakiii <tamakiii@users.noreply.github.com>
2019-12-03 23:14:06 +0900

    parametarize $CMD

--- test.mk
+++ test.mk
@@ -1,5 +1,7 @@
 .PHONY: all consistent delegate cached nkf clean
 
+CMD := php app/bin/console c:c
+
 all: \
 	consistent \
 	delegated \
@@ -10,26 +12,26 @@ consistent: clean
 	make -f dev.mk .env MOUNT_TYPE=$@
 	docker-compose config
 	docker-compose run --rm php make -C app install
-	docker-compose run --rm php time php app/bin/console c:c
+	docker-compose run --rm php time $(CMD)
 
 delegated: clean
 	make -f dev.mk .env MOUNT_TYPE=$@
 	docker-compose config
 	docker-compose run --rm php make -C app install
-	docker-compose run --rm php time php app/bin/console c:c
+	docker-compose run --rm php time $(CMD)
 
 cached: clean
 	make -f dev.mk .env MOUNT_TYPE=$@
 	docker-compose config
 	docker-compose run --rm php make -C app install
-	docker-compose run --rm php time php app/bin/console c:c
+	docker-compose run --rm php time $(CMD)
 
 nfs: clean
 	make -f dev.mk .env MOUNT_TYPE=consistent
 	make -f dev.mk docker-compose.override.yml
 	docker-compose config
 	docker-compose run --rm php make -C app install
-	docker-compose run --rm php time php app/bin/console c:c
+	docker-compose run --rm php time $(CMD)
 
 clean:
 	docker-compose down -v
