5804fedcd327e2052fb20719e7003bfa8f0cd778
tamakiii <tamakiii@users.noreply.github.com>
2019-12-04 03:52:41 +0900

    add default docker-compose.override.yml target

--- dev.mk
+++ dev.mk
@@ -18,6 +18,9 @@ all:
 	echo "CURRENT_DIR=$(DIR)" >> $@
 	echo "CURRENT_VOLUME_DIR=$(CURRENT_VOLUME_DIR)" >> $@
 
+docker-compose.override.yml: docker/docker-compose.override.yml
+	cp $< $@
+
 clean:
 	rm -rf .env
 	rm -rf docker-compose.override.yml
