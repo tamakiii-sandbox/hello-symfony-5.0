b176892da240272bbd0dd38211a9f755f9708a88
tamakiii <tamakiii@users.noreply.github.com>
2019-12-03 21:45:45 +0900

    rename

--- dev.mk
+++ dev.mk
@@ -4,7 +4,7 @@ DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 
 ENVIRONMENT := development
 PORT_HTTP := 8080
-NFS_DEVICE_DIR ?= $(shell echo $(DIR) | sed 's|/Users|/System/Volumes/Data/Users|')
+DEVICE_DIR ?= $(realpath $(shell echo $(DIR) | sed 's|/Users|/System/Volumes/Data/Users|'))
 
 all:
 	cat $(lastword $(MAKEFILE_LIST))
@@ -13,7 +13,7 @@ all:
 	touch $@
 	echo "ENVIRONMENT=$(ENVIRONMENT)" >> $@
 	echo "PORT_HTTP=$(PORT_HTTP)" >> $@
-	echo "NFS_DEVICE_DIR=$(NFS_DEVICE_DIR)" >> $@
+	echo "DEVICE_DIR=$(DEVICE_DIR)" >> $@
 
 clean:
 	rm -rf app
