6e45ff56fcb26b096ba17835d1cf772831c4e962
tamakiii <tamakiii@users.noreply.github.com>
2019-12-03 19:52:03 +0900

    fix for Catalina

--- bin/setup_native_nfs_docker_osx.sh
+++ bin/setup_native_nfs_docker_osx.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
+set -eu
+
+PWD=$(cd $(dirname $0)/..; pwd)
+
 OS=`uname -s`
+OS_VERSION_MAJOR=`sw_vers -productVersion | awk -F '.' '{ print $1 }'`
+OS_VERSION_MINOR=`sw_vers -productVersion | awk -F '.' '{ print $2 }'`
+
+U=`id -u`
+G=`id -g`
 
 if [ $OS != "Darwin" ]; then
   echo "This script is OSX-only. Please do not run it on any other Unix."
@@ -19,7 +28,6 @@ echo " +-----------------------------+"
 echo ""
 
 echo "WARNING: This script will shut down running containers."
-echo ""
 echo -n "Do you wish to proceed? [y]: "
 read decision
 
@@ -27,6 +35,49 @@ if [ "$decision" != "y" ]; then
   echo "Exiting. No changes made."
   exit 1
 fi
+
+echo ""
+
+echo "WARNING: This script will overwrite those files"
+echo "- ~/Library/LaunchAgents/com.apple.nfsd.plist"
+echo "- ~/Library/LaunchAgents/com.apple.nfsconf.plist"
+echo "- /usr/local/etc/exports"
+echo "- /usr/local/etc/nfs.conf"
+echo -n "Do you wish to proceed? [y]: "
+read decision
+
+if [ "$decision" != "y" ]; then
+  echo "Exiting. No changes made."
+  exit 1
+fi
+
+echo ""
+
+echo "== Creating /etc/local/etc/exports"
+echo "/System/Volumes/Data/Users/ -alldirs -mapall=$U:$G localhost" > /usr/local/etc/exports
+
+echo "== Creating /usr/local/etc/nfs.conf"
+cat <<EOF > /usr/local/etc/nfs.conf
+#
+# nfs.conf: the NFS configuration file
+#
+nfs.server.mount.require_resv_port = 0
+EOF
+
+echo "== Configuring NFS"
+cp /System/Library/LaunchDaemons/com.apple.nfsd.plist ~/Library/LaunchAgents/com.apple.nfsd.plist
+cp /System/Library/LaunchDaemons/com.apple.nfsconf.plist ~/Library/LaunchAgents/com.apple.nfsconf.plist
+
+plutil -replace Label -string 'com.apple.nfsd.localhost' ~/Library/LaunchAgents/com.apple.nfsd.plist
+plutil -replace Label -string 'com.apple.nfsconf.localhost' ~/Library/LaunchAgents/com.apple.nfsconf.plist
+
+plutil -replace KeepAlive.PathState -xml '<dict><key>/usr/local/etc/exports</key><true/></dict>' ~/Library/LaunchAgents/com.apple.nfsd.plist
+plutil -replace WatchPaths -xml '<array><string>/usr/local/etc/nfs.conf</string></array>' ~/Library/LaunchAgents/com.apple.nfsconf.plist
+
+launchctl load ~/Library/LaunchAgents/com.apple.nfsd.plist || true
+launchctl load ~/Library/LaunchAgents/com.apple.nfsconf.plist || true
+
+launchctl remove com.apple.nfsconf || true
 
 echo ""
 
@@ -39,28 +90,13 @@ open -a Docker
 while ! docker ps > /dev/null 2>&1 ; do sleep 2; done
 
 echo "== Stopping running docker containers..."
-docker-compose down > /dev/null 2>&1
-docker volume prune -f > /dev/null
+docker-compose down
+docker-compose rm -v --stop
 
 osascript -e 'quit app "Docker"'
 
 echo "== Resetting folder permissions..."
-U=`id -u`
-G=`id -g`
-sudo chown -R "$U":"$G" .
-
-echo "== Setting up nfs..."
-LINE="/Users -alldirs -mapall=$U:$G localhost"
-FILE=/etc/exports
-sudo cp /dev/null $FILE
-grep -qF -- "$LINE" "$FILE" || sudo echo "$LINE" | sudo tee -a $FILE > /dev/null
-
-LINE="nfs.server.mount.require_resv_port = 0"
-FILE=/etc/nfs.conf
-grep -qF -- "$LINE" "$FILE" || sudo echo "$LINE" | sudo tee -a $FILE > /dev/null
-
-echo "== Restarting nfsd..."
-sudo nfsd restart
+sudo chown -R "$U":"$G" $PWD
 
 echo "== Restarting docker..."
 open -a Docker
